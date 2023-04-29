4ebc773bf560f6adba02d78163b033a23f9b1f8d
luizarn <luizadriven@gmail.com>
2023-04-28 23:13:37 -0300

    feat: post booking

--- src/repositories/booking-repository/index.ts
+++ src/repositories/booking-repository/index.ts
@@ -9,16 +9,32 @@ async function findBookingUser(userId: number) {
   });
 }
 
-// async function createPayment(ticketId: number, params: PaymentParams) {
-//   return prisma.payment.create({
-//     data: {
-//       ticketId,
-//       ...params,
-//     },
-//   });
-// }
+async function createBooking(roomId: number, userId: number) {
+  return prisma.booking.create({
+    data: {
+      roomId,
+      userId,
+    },
+  });
+}
+
+async function checkBook(roomId: number) {
+  return prisma.room.findFirst({
+    where: { id: roomId },
+  });
+}
+async function listBookingsByRoomId(roomId: number) {
+  return prisma.booking.findMany({
+    where: { roomId },
+    include: { Room: true },
+  });
+}
+
 const bookingRepository = {
   findBookingUser,
+  createBooking,
+  checkBook,
+  listBookingsByRoomId,
 };
 
 export default bookingRepository;
--- src/services/booking-service/index.ts
+++ src/services/booking-service/index.ts
@@ -1,15 +1,29 @@
 import { notFoundError } from '@/errors';
+import { cannotListBookingError } from '@/errors/cannot-list-booking-error';
 import bookingRepository from '@/repositories/booking-repository';
+import enrollmentRepository from '@/repositories/enrollment-repository';
+import ticketsRepository from '@/repositories/tickets-repository';
 
-// async function checkTicket(userId: number) {
-//   const enrollment = await enrollmentRepository.findWithAddressByUserId(userId);
+async function createBooking(userId: number, roomId: number) {
+  const enrollment = await enrollmentRepository.findWithAddressByUserId(userId);
+  if (!enrollment) throw cannotListBookingError();
 
-//   const ticket = await ticketsRepository.findTicketByEnrollmentId(enrollment.id);
+  const ticket = await ticketsRepository.findTicketByEnrollmentId(enrollment.id);
 
-//   if (!ticket || ticket.status === 'RESERVED' || ticket.TicketType.isRemote || !ticket.TicketType.includesHotel) {
-//     throw cannotListBookingError();
-//   }
-// }
+  if (!ticket || ticket.status === 'RESERVED' || ticket.TicketType.isRemote || !ticket.TicketType.includesHotel) {
+    throw cannotListBookingError();
+  }
+
+  const room = await bookingRepository.checkBook(roomId);
+  if (!room) throw notFoundError();
+
+  const quantityRoomByBooking = await bookingRepository.listBookingsByRoomId(userId);
+  if (room.capacity === quantityRoomByBooking.length) throw cannotListBookingError;
+
+  const bookingCreated = await bookingRepository.createBooking(userId, roomId);
+
+  return bookingCreated;
+}
 
 async function getBookings(userId: number) {
   const bookings = await bookingRepository.findBookingUser(userId);
@@ -19,6 +33,7 @@ async function getBookings(userId: number) {
 
 const bookingService = {
   getBookings,
+  createBooking,
 };
 
 export default bookingService;
